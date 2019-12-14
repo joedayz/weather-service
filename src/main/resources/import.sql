a9cda21b6aee12cf90a09bc1ed1def66bfed209c
Jose Diaz <jadiaz@farmaciasperuanas.pe>
2019-12-14 12:46:31 -0500

    codigo de weather-service

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+
+
+
+INSERT INTO `joedayz`.`weather` (`city`, `description`, `icon`) VALUES ('Paris, France', 'Very cloudy!', 'weather-fog');
+INSERT INTO `joedayz`.`weather` (`city`, `description`, `icon`) VALUES ('London, UK', 'Quite cloudy', 'weather-pouring');
